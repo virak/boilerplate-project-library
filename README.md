ad1fe88b553dfee62103bf87f4e87fb971299570
Virak Sun <sun.virak@gmail.com>
2021-09-15 10:34:26 +0200

    core: add connection to mongo atlas and https server configuration in server.js file

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 
 This fork is used for the implementation of the solution for this project using localhost and ngrok for exposing the app pubicly
 
+Make sure you have the self signed certificate files for the https to be able to run
 
+
+Here is the command for running ngrok:
+./ngrok http https://localhost:3000
+
+
+Make sure that the .env file contains the variable for mongo db atlas connection
--- server.js
+++ server.js
@@ -3,11 +3,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const https = require('https')
+const mongoose = require('mongoose')
+const fs = require('fs')
 require('dotenv').config()
+
+const key = fs.readFileSync('selfsigned.key')
+const cert = fs.readFileSync('selfsigned.crt')
+const options = {
+  key: key,
+  cert: cert
+}
 
 const apiRoutes = require('./routes/api.js')
 const fccTestingRoutes = require('./routes/fcctesting.js')
 const runner = require('./test-runner')
+
+// -- Mongo db Atlas connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log('> connection db ✅')
 
 const app = express()
 
@@ -37,20 +51,26 @@ app.use(function (req, res, next) {
     .send('Not Found')
 })
 
-// Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port)
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...')
-    setTimeout(function () {
-      try {
-        runner.run()
-      } catch (e) {
-        console.log('Tests are not valid:')
-        console.error(e)
-      }
-    }, 1500)
-  }
+const server = https.createServer(options, app)
+
+server.listen(3000, () => {
+  console.log('Server https listening on port : ' + 3000)
 })
+
+// Start our server and tests!
+// const listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log('Your app is listening on port ' + listener.address().port)
+//   if (process.env.NODE_ENV === 'test') {
+//     console.log('Running Tests...')
+//     setTimeout(function () {
+//       try {
+//         runner.run()
+//       } catch (e) {
+//         console.log('Tests are not valid:')
+//         console.error(e)
+//       }
+//     }, 1500)
+//   }
+// })
 
 module.exports = app // for unit/functional testing
